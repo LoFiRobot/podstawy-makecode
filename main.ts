b6798e3c4125fe82185768e53a8660d3c8deac7f
LOFI Robot <halo@lofirobot.com>
2020-10-27 12:36:41 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+basic.forever(function () {
+	
+})
